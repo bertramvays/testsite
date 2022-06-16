0132553e647aa00353cf25ac6175ff5381451010
Dmitrij <grenko_d@protonmail.com>
2022-06-16 19:09:15 +0300

    Maked migrations and created debug path for media files

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.Charfield(max_length=150)
+    title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanFields(default=True)
-
+    is_published = models.BooleanField(default=True)
