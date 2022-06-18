62df1aa8da588874435cca1dc5ec68118196c942
Dmitrij <grenko_d@protonmail.com>
2022-06-18 13:15:24 +0300

    Maked better view of admin. Created new model with foreign key (category for news). Studing tags

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    """Класс для того, какие поля показывать в админке"""
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')
-    search_field = ('title', 'content')
+    search_field = ('title', 'content')  # искать по этому полю
+    list_editable = ('is_published',)  # редактировать это поле в меню админки
+    list_filter = ('is_published', 'category')
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Класс для того, какие поля показывать в админке"""
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_field = ('title',)  # здесь должен быть кортеж, поэтому запятая в конце
+
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
