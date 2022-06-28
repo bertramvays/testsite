81f112341b5a7076a980d0d297c856a21ee5b60d
Dmitrij <grenko_d@protonmail.com>
2022-06-28 16:55:21 +0300

    Studing DjangoOrm

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -8,7 +8,9 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновленно')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано?')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')  #  related_name='get_news' я переназначения news_set в ОРМ
+    views = models.IntegerField(default=0)
+
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"pk": self.pk})
